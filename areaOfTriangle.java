e9522401e46aa43c1039ee3adaf6194612d68f7b
NathanM1803 <114627379+NathanM1803@users.noreply.github.com>
2022-10-08 15:19:27 -0400

    Add files via upload

---
+++ areaOfTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class areaOfTriangle {
+    public static void main(String[] args){
+
+        double s,Area;
+    Scanner keyboard = new Scanner(System.in); 
+
+        System.out.print("Enter length of side a " );
+            double a = keyboard.nextInt();
+
+        System.out.print("Enter length of side b " );
+            double b = keyboard.nextInt();
+
+        System.out.print("Enter length of side c ");
+            double c = keyboard.nextInt();
+
+        s = (a+b+c)/2.0d;
+        Area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+
+        System.out.format("Area of traingle is = %.3f\n", Area);
+    }
+    
+}
